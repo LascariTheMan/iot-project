6d19458d942f41cdf2c49ddd70eed09c22130da5
Unknown <mads1108@hotmail.dk>
2019-04-19 14:12:45 +0200

    mqtt app

--- js/main.js
+++ js/main.js
@@ -1,4 +1,42 @@
 let findBtn = document.getElementById('findBtn');
+let lostBtn = document.getElementById('lostBtn');
+
+client = new Paho.MQTT.Client("m24.cloudmqtt.com", 14379, 'web_niels');
+
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = plotCupOnMap;
+
+var options = {
+    useSSL: true,
+    userName: "mhyidsth",
+    password: "sQBr9o6btgzV",
+    onSuccess: onConnect,
+    onFailure: doFail
+};
+
+client.connect(options);
+
+
+function onConnect() {
+    console.log('Connected succesfully to cloudmqtt!')
+}
+
+function doFail(e) {
+    console.log(e)
+};
+
+function onConnectionLost() {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:"+responseObject.errorMessage);
+      }
+}
+
+
+function cupLost() {
+    message = new Paho.MQTT.Message();
+    message.destinationName = "/cup";
+    client.send(message);
+}
 
 findBtn.onclick = plotCupOnMap;
 
